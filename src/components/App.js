5a4ecbd19b521d627eaace0498b97be968bbad3d
ROSHAN KUMAR JHA <73186190+roshan837@users.noreply.github.com>
2022-08-16 13:40:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,33 @@
-import React, {useState } from "react";
+import React, {useState,useRef } from "react";
 import "../styles/App.css";
 
 export default function App(){
-  
+  const interval = useRef();
+  const move = useRef();
   const [x,setX]=useState(0);
   const [y,setY]=useState(0);
   const [time,setTime]=useState(0);
-  const [me,setMe]=useState(false);
-  const [count,setCount]=useState(0);
-const my=()=>setInterval(()=>setTime(i=>i+1),1000);
-const move = (e)=>{
-  if(e.key==='ArrowUp')setY(y-5);
-  if(e.key==='ArrowDown')setY(y+5);
-  if(e.key==='ArrowLeft')setX(x-5);
-  if(e.key==='ArrowRight')setX(x+5);
-  console.log(e.key,x,y)
-  if(x===250 && y===250){setMe(false);setCount(time);
+console.log(x,y,interval.current)
+if(x===250 && y===250){
+  clearInterval(interval.current);
+  window.removeEventListener('keydown',move.current)
 }
+const start = () => {
+  move.current = (e) =>{
+    if(e.key==='ArrowUp')setY(y=>y-5);
+    else if(e.key==='ArrowDown')setY(y=>y+5);
+    else if(e.key==='ArrowLeft')setX(x=>x-5);
+    else if(e.key==='ArrowRight')setX(x=>x+5);
+  }
+  interval.current=setInterval(()=>setTime(time=>time+1),1000);
+  window.addEventListener('keydown',move.current);
 }
-if(me===true && x!==250 && y!==250)window.addEventListener('keydown',(e)=>move(e));
-
   return (
    <>
    <div className="ball" style={{top: y,left: x}}></div>
    <div className="hole"></div>
-   <div className="heading-timer">{me? time:count}</div>
-   <button className="start" onClick={()=>{
-    my()
-    setMe(!me);
-  }}>start</button>
+   <div className="heading-timer">{time}</div>
+   <button className="start" onClick={()=>start()}>start</button>
   </>
       )
     }
