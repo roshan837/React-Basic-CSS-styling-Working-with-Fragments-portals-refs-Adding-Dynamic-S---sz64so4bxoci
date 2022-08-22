18d7782436d3d4f191d058c4714d2fa743592956
ROSHAN KUMAR JHA <73186190+roshan837@users.noreply.github.com>
2022-08-22 13:36:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,22 @@ import "../styles/App.css";
 
 export default function App(){
   const interval = useRef();
+  const [hole,setHole]=useState(250)
   const move = useRef();
   const [x,setX]=useState(0);
   const [y,setY]=useState(0);
   const [time,setTime]=useState(0);
-if(x===250 && y===250){
+if(x===hole && y===hole){
   clearInterval(interval.current);
   window.removeEventListener('keydown',move.current)
 }
 const start = () => {
+  clearInterval(interval.current);
+  window.removeEventListener('keydown',move.current)
   setTime(0);
   setX(0);
   setY(0);
+  setHole(100+Math.floor(Math.random()*100)*5)
   move.current = (e) =>{
     if(e.key==='ArrowUp')setY(y=>y-5);
     if(e.key==='ArrowDown')setY(y=>y+5);
@@ -26,8 +30,8 @@ const start = () => {
 }
   return (
    <>
-   <div className="ball" style={{top: `${y}px`,left: `${x}px`}}></div>
-   <div className="hole"></div>
+   <div className="ball" style={{top: y,left: x}}></div>
+   <div className="hole" style={{top: hole,left: hole}}></div>
    <div className="heading-timer">{time}</div>
    <button className="start" onClick={()=>start()}>start</button>
   </>
